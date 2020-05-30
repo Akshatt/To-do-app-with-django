5a8e105910244c743cdad1203a35b5c5a58a0d36
Akshat Jain <jain.akshat31@gmail.com>
2020-05-31 02:39:53 +0530

    functionality for adding items to list

--- main/views.py
+++ main/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Todo
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
-def index(request):
-    return render(request, 'main/index.html')
-
 def home(request):
-    return render(request, 'main/index.html')
+    todo_list = Todo.objects.all().order_by("-added_date")
+    context = {'todo_list': todo_list}
+    return render(request, 'main/index.html', context=context)
 
+
+def add_todo(request):
+    current_date = timezone.now()
+    content = request.POST["content"]
+    obj = Todo.objects.create(added_date=current_date, text=content)
+    return HttpResponseRedirect("/")
